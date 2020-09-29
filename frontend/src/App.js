b1f0241356c4d674e169f27900e568775219fea5
luanbarboza <luanbarboza2009@hotmail.com>
2020-09-29 17:20:48 -0300

    axios config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './components/Header';
+import api from './services/api';
+
+import './App.css';
+import backgroundImage from './assets/background.jpg'
 
 function App(){
-    return <h1>Hello Word</h1>;
+    const [projects, setProjects]  = useState(['Desenvolvimento de app', 'Front-end web']);
+
+    function handleAddProject() {
+        // projects.push(`novo projeto ${Date.now()}`);
+        setProjects([... projects, `novo projeto ${Date.now()}`]);
+        console.log(projects);
+    }
+
+    return (
+        <>
+     
+        <Header title="Projects"/>
+
+         <ul>
+            {projects.map(project => <li key={project}>{project}</li>)}
+         </ul>
+
+         <button type="button" onClick={handleAddProject}>Adicionar projeto</button>
+        </>
+    );
 }
 
 export default App;
