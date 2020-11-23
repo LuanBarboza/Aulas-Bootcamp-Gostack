b59ee329d5ce617d6f3e411e415c58640aa43a9d
Luan <luanbarboza2009@hotmail.com>
2020-11-22 23:24:43 -0300

    Add mobile

---
+++ mobile/src/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { StatusBar, View, StyleSheet, Text } from 'react-native';
+
+export default function App (){
+    return (
+        <>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+
+        <View style={styles.container}>
+            <Text style={styles.title}>Hello GoStack</Text>
+        </View>
+        </>
+    
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#7159c1',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    title: {
+        color: '#FFF',
+        fontSize: 32,
+        fontWeight: 'bold'
+    },
+});
+
